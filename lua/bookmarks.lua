1b1bac708ad48840221163a8fa7e50becba3792e
Geertsky <122536131+Geertsky@users.noreply.github.com>
2023-09-27 21:21:04 +0200

    initial version

--- lua/bookmarks.lua
+++ lua/bookmarks.lua
@@ -1,10 +1,13 @@
-function setup(opts)
-  local bmpath = opts.bookmarkspath
-  if bookmarkspath == nil then
-    bmpath = os.getenv( "HOME" ) .. '.bookmarks.lua'
-  end
-  bookmarks = dofile(bookmarkspath)
+local M = {}
+M.config = {
+  bookmarkspath = os.getenv( "HOME" ) .. '/.bookmarks.lua',
+}
+M.setup = function(args)
+  M.config = vim.tbl_deep_extend("force", M.config, args or {})
+  local bookmarks = dofile(M.config.bookmarkspath)
   for key,path in pairs(bookmarks) do
     vim.keymap.set('n', '<leader>'..key, ':cd '..path..'<cr>')
   end
 end
+
+return M
