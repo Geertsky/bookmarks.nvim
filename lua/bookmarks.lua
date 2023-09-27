1e522628cb4c9860364a7ee203d892d41f42de93
Geertsky <122536131+Geertsky@users.noreply.github.com>
2023-09-27 13:47:43 +0200

    restores lua file

---
+++ lua/bookmarks.lua
@@ -0,0 +1,10 @@
+function setup(opts)
+  local bmpath = opts.bookmarkspath
+  if bookmarkspath == nil then
+    bmpath = os.getenv( "HOME" ) .. '.bookmarks.lua'
+  end
+  bookmarks = dofile(bookmarkspath)
+  for key,path in pairs(bookmarks) do
+    vim.keymap.set('n', '<leader>'..key, ':cd '..path..'<cr>')
+  end
+end
