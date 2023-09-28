e7ee1866e658d6c8c988a45df0f451fa56ccb0f6
Geertsky <122536131+Geertsky@users.noreply.github.com>
2023-09-28 14:36:31 +0200

    Added some doku

--- etc/profile.d/bookmarks.sh
+++ etc/profile.d/bookmarks.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+#GG: BOOKMARKSPATH HAS to be identical to the bookmarkspath within nvim
 BOOKMARKSPATH=/home/geert/.bookmarks.lua
+#GG: an optional prefix for the aliases because of simpler bash completion
 PREFIX="gg"
 while read k v; do
    if ! alias "$PREFIX""$k" >/dev/null 2>&1; then
