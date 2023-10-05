b74ab8824ecfff254c80cdc026f20c697f52914d
Geertsky <122536131+Geertsky@users.noreply.github.com>
2023-10-05 12:06:32 +0200

    Add which-key description

--- lua/bookmarks.lua
+++ lua/bookmarks.lua
@@ -1,12 +1,14 @@
 local M = {}
 M.config = {
-  bookmarkspath = os.getenv( "HOME" ) .. '/.bookmarks.lua',
+  bookmarkspath = os.getenv("HOME") .. '/.bookmarks.lua',
 }
 M.setup = function(args)
   M.config = vim.tbl_deep_extend("force", M.config, args or {})
   local bookmarks = dofile(M.config.bookmarkspath)
-  for key,path in pairs(bookmarks) do
-    vim.keymap.set('n', '<leader>'..key, ':cd '..path..'<cr>')
+  for key, path in pairs(bookmarks) do
+    local kf = key:sub(2)
+    vim.keymap.set('n', '<leader>' .. key, ':cd ' .. path .. '<cr>',
+      { desc = '[C]d ' .. path:gsub('/' .. kf, '/[' .. string.upper(kf) .. ']', 1) })
   end
 end
 
